523ad36e92448ee0cb37ff6df53b9f3f4fadf9c9
dcoletto <d.coletto@eggonemail.com>
2019-04-18 01:58:55 +0200

    token_type and expires_at aren't mandatory anymore

--- library/java/net/openid/appauth/TokenResponse.java
+++ library/java/net/openid/appauth/TokenResponse.java
@@ -213,9 +213,13 @@ public class TokenResponse {
          */
         @NonNull
         public Builder fromResponseJson(@NonNull JSONObject json) throws JSONException {
-            setTokenType(JsonUtil.getString(json, KEY_TOKEN_TYPE));
+            if(json.has(KEY_TOKEN_TYPE)){
+                setTokenType(JsonUtil.getString(json, KEY_TOKEN_TYPE));
+            }
             setAccessToken(JsonUtil.getStringIfDefined(json, KEY_ACCESS_TOKEN));
-            setAccessTokenExpirationTime(JsonUtil.getLongIfDefined(json, KEY_EXPIRES_AT));
+            if (json.has(KEY_EXPIRES_AT)) {
+                setAccessTokenExpirationTime(JsonUtil.getLongIfDefined(json, KEY_EXPIRES_AT));
+            }
             if (json.has(KEY_EXPIRES_IN)) {
                 setAccessTokenExpiresIn(json.getLong(KEY_EXPIRES_IN));
             }
